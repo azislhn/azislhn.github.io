991f076185ee521f5a77614757183baa9a26eed9
Aziz Solihin <azizslhn45@gmail.com>
2022-12-21 01:55:01 +0700

    update

--- assets/newScript.js
+++ assets/newScript.js
@@ -1,12 +1,14 @@
 $(document).ready(() => {
-  $('.mode-toggler').hover(() => {
-    $('.mode-toggler i').toggleClass('fa-lg')
-  })
+  date();
 
-  $('.mode-toggler').click(() => {
-    $('#sun').toggleClass('hide')
-    $('#moon').toggleClass('hide')
-  })
+  $(".mode-toggler").hover(() => {
+    $(".mode-toggler i").toggleClass("fa-lg");
+  });
+
+  $(".mode-toggler").click(() => {
+    $("#sun").toggleClass("hide");
+    $("#moon").toggleClass("hide");
+  });
 
   document.querySelectorAll(".nav-link").forEach((link) => {
     $(link).on("click", () => {
@@ -21,26 +23,50 @@ $(document).ready(() => {
 
 window.onscroll = () => {
   let curr = "";
-  document.querySelectorAll('section').forEach((section) => {
+  document.querySelectorAll("section").forEach((section) => {
     const sectionTop = section.offsetTop;
     if (scrollY >= sectionTop - 240) {
-      curr = section.getAttribute("id")
-      // console.log(curr);
+      curr = section.getAttribute("id");
     }
-  })
+  });
   document.querySelectorAll(".navlinks .nav-link").forEach((link) => {
-    // console.log(link);
     link.classList.remove("active");
     if (link.classList.contains(curr)) {
       link.classList.add("active");
     }
   });
-}
+};
 
-function clock() {
-  let time = new Date()
-  time = time.toUTCString()
+function date() {
+  const datetime = new Date();
 
-  $('#timer').html("<small>"+time+"</small>")
-  setTimeout(clock, 1000)
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const day = days[datetime.getDay()];
+  const date = datetime.getDate();
+  const month = months[datetime.getMonth()];
+  const year = datetime.getFullYear();
+
+  $("#date").html(`<small>${day}, ${date} ${month} ${year}</small>`);
 }
